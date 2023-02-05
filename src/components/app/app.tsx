c8ad5f04d180bfbcc7ed87f840aecfbc02415c91
Basnor <basnor@mail.ru>
2023-02-05 11:48:25 +0700

    move app

---
+++ src/components/app/app.tsx
@@ -0,0 +1,18 @@
+import styles from './app.module.css';
+import AppHeader from '../app-header/app-header';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <main>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </main>
+    </div>
+  );
+}
+
+export default App;
