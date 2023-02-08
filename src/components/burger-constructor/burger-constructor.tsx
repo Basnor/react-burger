b0487fb29c5c0f590c1c9d6ff410fcd6513165fa
Basnor <basnor@mail.ru>
2023-02-08 23:11:59 +0700

    fix burgerConstructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -7,48 +7,47 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./burger-constructor.module.css";
-import { ingredients } from "../../utils/data";
+import { IIngredient, IngredientType } from "../../utils/types";
+import { INGREDIENTS_URL } from "../../utils/contants";
+import useFetch from "../../hooks/useFetch";
 
-function BurgerConstructorBons(props: {
-  ingredient: any;
-  children: ReactNode;
-}) {
+function BurgerConstructorBuns(props: { ingredient: IIngredient|undefined; children: ReactNode; }) {
   const { ingredient, children } = props;
 
   return (
-    <>
-      <ConstructorElement
-        type="top"
-        isLocked={true}
-        text={`${ingredient.name} (верх)`}
-        price={ingredient.price}
-        thumbnail={ingredient.image}
-        extraClass="ml-8 mr-2"
-      />
-      {children}
-      <ConstructorElement
-        type="bottom"
-        isLocked={true}
-        text={`${ingredient.name} (низ)`}
-        price={ingredient.price}
-        thumbnail={ingredient.image}
-        extraClass="ml-8 mr-2"
-      />
-    </>
+      ingredient
+      ? <>
+        <ConstructorElement
+          type="top"
+          isLocked={true}
+          text={`${ingredient.name} (верх)`}
+          price={ingredient.price}
+          thumbnail={ingredient.image}
+          extraClass="ml-8 mr-2"
+        />
+        {children} 
+        <ConstructorElement
+          type="bottom"
+          isLocked={true}
+          text={`${ingredient.name} (низ)`}
+          price={ingredient.price}
+          thumbnail={ingredient.image}
+          extraClass="ml-8 mr-2"
+        />
+      </>
+      : <>{children}</>
   );
 }
 
 function BurgerConstructor() {
+  const { isLoading, data, error } = useFetch<IIngredient>(INGREDIENTS_URL);
+  
   return (
     <div className={`ml-4 mr-4 mt-25 ${styles.wrapper}`}>
       <div className={styles.layers}>
-        <BurgerConstructorBons
-          ingredient={ingredients.find(
-            (ingredient) => ingredient.type === "bun"
-          )}
-        >
+        <BurgerConstructorBuns ingredient={data.find(({type}) => type as IngredientType === IngredientType.Bun)}>
           <ul className={styles.toppings}>
-            {ingredients.filter((ingredient) => ingredient.type !== "bun").map((ingredient) => {
+            {data.filter(({type}) => type !== IngredientType.Bun).map((ingredient) => {
                 return (
                   <li key={ingredient._id} className={styles.topping}>
                     <DragIcon type="primary" />
@@ -62,7 +61,7 @@ function BurgerConstructor() {
                 );
               })}
           </ul>
-        </BurgerConstructorBons>
+        </BurgerConstructorBuns>
 
         <div className={`mt-10 mb-10 mr-4 ${styles.pricing}`}>
           <span className="text text_type_digits-medium mr-2">12390</span>
