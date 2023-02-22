ef54050e7efc6ec100b44f879a89dc5d4245d643
Basnor <basnor@mail.ru>
2023-02-22 23:52:21 +0700

    refact pricing

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useReducer } from "react";
+import React from "react";
 import { useDrop } from "react-dnd";
-import {
-  CurrencyIcon,
-  Button,
-} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import { nanoid } from "nanoid";
 
 import styles from "./burger-constructor.module.css";
@@ -14,64 +11,19 @@ import { RootState } from "../../services";
 import BurgerConstructorToppings from "./components/burger-constructor-toppings";
 import { addBurgerIngredient } from "../../services/burger-constructor";
 import { createOrder } from "../../services/order-details";
-
-enum Action {
-  ADD_BUNS = "ADD_BUNS",
-  ADD_TOPPINGS = "ADD_TOPPINGS",
-  CLEAR = "CLEAR",
-}
-
-type TotalPriceStateProps = {
-  price: number;
-};
-
-type TotalPriceActionProps = {
-  type: Action;
-  payload?: {
-    price: number;
-  };
-};
-
-const totlPriceInitialState: TotalPriceStateProps = { price: 0 };
-
-function totalPriceReducer(
-  state: TotalPriceStateProps,
-  action: TotalPriceActionProps
-) {
-  switch (action.type) {
-    case Action.ADD_BUNS:
-      if (!action.payload) {
-        return { price: state.price };
-      }
-
-      return { price: state.price + action.payload.price * 2 };
-
-    case Action.ADD_TOPPINGS:
-      if (!action.payload) {
-        return { price: state.price };
-      }
-
-      return { price: state.price + action.payload.price };
-
-    case Action.CLEAR:
-      return totlPriceInitialState;
-
-    default:
-      throw new Error(`Wrong type of action: ${action.type}`);
-  }
-}
+import BurgerConstructorPrice from "./components/burger-constructor-price";
+import BurgerConstructorEmptyState from "./components/burger-constructor-empty-state";
 
 function BurgerConstructor() {
   const dispatch = useAppDispatch();
 
   const { toppings, bun } = useAppSelector((store: RootState) => store.burgerConstructor);
-  const [totalPriceState, totalPriceDispatcher] = useReducer(
-    totalPriceReducer,
-    totlPriceInitialState
-  );
 
-  const [, dropRef] = useDrop({
+  const [{ isDragging }, dropRef] = useDrop({
     accept: DragType.Ingredient,
+    collect: (monitor) => ({
+      isDragging: monitor.canDrop(),
+    }),
     drop: (ingredient: IIngredient) =>
       dispatch(
         addBurgerIngredient({
@@ -82,16 +34,6 @@ function BurgerConstructor() {
         })
       ),
   });
-
-  useEffect(() => {
-    totalPriceDispatcher({ type: Action.CLEAR });
-
-    toppings.map(({ price }) => {
-      totalPriceDispatcher({ type: Action.ADD_TOPPINGS, payload: { price } });
-    });
-
-    totalPriceDispatcher({ type: Action.ADD_BUNS, payload: { price: bun?.price || 0 } });
-  }, [toppings, bun]);
 
   const handleCreateOrder = () => {
     const buns = bun ? [bun, bun] : [];
@@ -112,23 +54,22 @@ function BurgerConstructor() {
           <BurgerConstructorBuns>
             <BurgerConstructorToppings />
           </BurgerConstructorBuns>
-
+          {(toppings.length || bun) && (
+            <div className={`${styles.pricing} mt-10 mr-8`}>
+              <BurgerConstructorPrice />
+              <Button
+                htmlType="button"
+                type="primary"
+                size="large"
+                extraClass="ml-10"
+                onClick={handleCreateOrder}
+              >
+                Оформить заказ
+              </Button>
+            </div>
+          )}
         </div>
-          <div className={`${styles.pricing} mt-10 mb-10 mr-4`}>
-            <span className="text text_type_digits-medium mr-2">
-              {totalPriceState.price}
-            </span>
-            <CurrencyIcon type="primary" />
-            <Button
-              htmlType="button"
-              type="primary"
-              size="large"
-              extraClass="ml-10"
-              onClick={handleCreateOrder}
-            >
-              Оформить заказ
-            </Button>
-          </div>
+        <BurgerConstructorEmptyState isDragging={isDragging} />
       </div>
     </>
   );
