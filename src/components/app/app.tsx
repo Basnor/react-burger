6a6051b662d949a511b605f8840351ea835cb82d
Basnor <basnor@mail.ru>
2023-05-04 04:32:11 +0700

    gh

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import Home from "../../pages/home";
 import Feed from "../../pages/feed";
@@ -18,104 +18,104 @@ import ProfileFeed from "../../features/feed/profile-feed";
 import OrderDetails from "../order-details/order-details";
 import { ROUTES } from "../../utils/contants";
 
-const router = createBrowserRouter([
-  {
-    path: ROUTES.HOME,
-    element: <BaseLayout />,
-    children: [
-      {
-        path: ROUTES.HOME,
-        element: <Home />,
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            path: ROUTES.INGREDIENT,
-            element: <IngredientDetails />,
-          },
-        ]
-      },
-      {
-        path: ROUTES.FEED,
-        element: <Feed />,
-        children: [
-          {
-            path: ROUTES.ORDER,
-            element: <OrderDetails />,
-          },
-        ]
-      },
-      {
-        path: ROUTES.LOGIN,
-        element: (
-          <ProtectedRoute role={Role.GUEST}>
-            <Login />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: ROUTES.REGISTER,
-        element: (
-          <ProtectedRoute role={Role.GUEST}>
-            <Register />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: ROUTES.FORGOT_PASSWORD,
-        element: (
-          <ProtectedRoute role={Role.GUEST}>
-            <ForgotPassword />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: ROUTES.RESET_PASSWORD,
-        element: (
-          <ProtectedRoute role={Role.GUEST}>
-            <ResetPassword />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: ROUTES.PROFILE,
-        element: (
-          <ProtectedRoute role={Role.USER}>
-            <Profile />
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            path: ROUTES.PROFILE,
-            element: <ProfileDetails />,
-          },
-          {
-            path: ROUTES.ORDERS,
-            element: <ProfileFeed />,
-            children: [
-              {
-                path: ROUTES.PROFILE_ORDER,
-                element: <OrderDetails />,
-              },
-            ]
-          },
-          {
-            path: ROUTES.LOGOUT,
-            element: <Logout />,
-          },
-        ],
-      },
-      {
-        path: "*",
-        element: <ErrorPage />,
-      },
-    ],
-  },
-]);
+const router = createHashRouter(
+  [
+    {
+      path: ROUTES.HOME,
+      element: <BaseLayout />,
+      children: [
+        {
+          path: ROUTES.HOME,
+          element: <Home />,
+          errorElement: <ErrorPage />,
+          children: [
+            {
+              path: ROUTES.INGREDIENT,
+              element: <IngredientDetails />,
+            },
+          ],
+        },
+        {
+          path: ROUTES.FEED,
+          element: <Feed />,
+          children: [
+            {
+              path: ROUTES.ORDER,
+              element: <OrderDetails />,
+            },
+          ],
+        },
+        {
+          path: ROUTES.LOGIN,
+          element: (
+            <ProtectedRoute role={Role.GUEST}>
+              <Login />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: ROUTES.REGISTER,
+          element: (
+            <ProtectedRoute role={Role.GUEST}>
+              <Register />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: ROUTES.FORGOT_PASSWORD,
+          element: (
+            <ProtectedRoute role={Role.GUEST}>
+              <ForgotPassword />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: ROUTES.RESET_PASSWORD,
+          element: (
+            <ProtectedRoute role={Role.GUEST}>
+              <ResetPassword />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: ROUTES.PROFILE,
+          element: (
+            <ProtectedRoute role={Role.USER}>
+              <Profile />
+            </ProtectedRoute>
+          ),
+          children: [
+            {
+              path: ROUTES.PROFILE,
+              element: <ProfileDetails />,
+            },
+            {
+              path: ROUTES.ORDERS,
+              element: <ProfileFeed />,
+              children: [
+                {
+                  path: ROUTES.PROFILE_ORDER,
+                  element: <OrderDetails />,
+                },
+              ],
+            },
+            {
+              path: ROUTES.LOGOUT,
+              element: <Logout />,
+            },
+          ],
+        },
+        {
+          path: "*",
+          element: <ErrorPage />,
+        },
+      ],
+    },
+  ]
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
